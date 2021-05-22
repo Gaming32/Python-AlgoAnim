7a45794ba80b29f3fd58f719c3d42ec11580c546
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-05-21 19:02:22 -0500

    Update bubble.py

--- algoanim/sorts/bubble.py
+++ algoanim/sorts/bubble.py
@@ -6,7 +6,7 @@ class BubbleSort(Sort):
     name = 'Bubble Sort'
 
     def run(self, array: Array) -> None:
-        for i in range(len(array) - 1, -1, -1):
+        for i in range(len(array), 0, -1):
             for j in range(1, i):
                 if array[j - 1] > array[j]:
                     array.swap(j - 1, j)
